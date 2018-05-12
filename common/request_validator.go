cd80de57d0594b01fdb0b5d222faf87bc91ccd53
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2018-05-12 11:42:15 +0300

    Add request validator action

---
+++ common/request_validator.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"net/http"
+
+	"github.com/TsvetanMilanov/go-lambda-workflow/workflow"
+	"github.com/go-playground/validator"
+)
+
+// ValidateRequest validates the request object in the context and sets the
+// response to bad request if there are any errors.
+func ValidateRequest(ctx workflow.Context) error {
+	req := ctx.GetRequest()
+	vErrs := validateRequest(req)
+
+	if len(vErrs) > 0 {
+		resBody := []string{}
+		for _, e := range vErrs {
+			resBody = append(resBody, e.Translate(nil))
+		}
+
+		ctx.SetResponse(resBody).SetResponseStatusCode(http.StatusBadRequest)
+	}
+
+	return nil
+}
+
+func validateRequest(req interface{}) validator.ValidationErrors {
+	validate := validator.New()
+	errs := validate.Struct(req)
+	return errs.(validator.ValidationErrors)
+}
